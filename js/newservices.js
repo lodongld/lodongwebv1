589acb1106aa855f673d761a7a0f0f08174a48cd
Christian Kim Abac <abackim9087@gmail.com>
2023-03-27 21:45:31 +0800

    update fix

--- js/newservices.js
+++ js/newservices.js
@@ -3,22 +3,24 @@ $(".services").hover(function(){
     $(this).removeClass('col-xxl-1');
     $(this).removeClass('col-lg-1');
 
-    $(this).addClass('col-xxl-4');
+    $(this).addClass('col-xxl-4',function(){
+      $(this).find('.tag').removeClass('d-bock').hide();
+      $(this).find('.tag-desc').removeClass('d-none').hide();
+      $(this).find('.tag-desc').delay('400').fadeIn('slow');
+    });
     $(this).addClass('col-lg-4');
     $(this).addClass('mobile-expand');
 
-    $(this).find('.tag').removeClass('d-bock').hide();
-    $(this).find('.tag-desc').removeClass('d-none').hide();
-    $(this).find('.tag-desc').delay('350').fadeIn('slow');
+    
 
   }, function() {
-    $(this).removeClass('col-xxl-4');
+    $(this).removeClass('col-xxl-4',function(){
+      $(this).find('.tag').addClass('d-bock').show();
+      $(this).find('.tag-desc').addClass('d-none');
+    });
     $(this).removeClass('col-lg-4');
     $(this).removeClass('mobile-expand');
 
     $(this).addClass('col-xxl-1');
-    $(this).addClass('col-lg-1');
-
-    $(this).find('.tag').addClass('d-bock').show();
-    $(this).find('.tag-desc').addClass('d-none');
+    $(this).addClass('col-lg-1');    
   });
